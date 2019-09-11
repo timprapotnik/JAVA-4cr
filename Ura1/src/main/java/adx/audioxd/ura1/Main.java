07fbe4ccfe140cbf2df21182fe9fda3f75dd1320
tim.prapotnik <tim.prapotnik@sers.si>
2019-09-11 14:19:00 +0200

    Change

--- Ura1/src/main/java/adx/audioxd/ura1/Main.java
+++ Ura1/src/main/java/adx/audioxd/ura1/Main.java
@@ -1,7 +1,20 @@
 package adx.audioxd.ura1;
 
+
+/**
+ * 1. Na USB namesti okolje Eclipse
+ * 2. V zvezek zapiši program Naloga 1
+ * 3. V zvezek zapiši program Naloga 2
+ *
+ * Naloge:
+ * 1. Napiši program ki izpiše "Uspeh ni naklučen!"
+ * 2. Napiši program Naloga_2_Pozdrav, ki izpiše: "Uspeh ni naklučen!" in "To je SERŠ!". Ta
+ * program mora imeti poseben razred izpis tekstov na ekran
+ */
+
 public class Main {
     public static void main(String... args) {
-        System.out.println("Uspeh ni naklučen.");
+        System.out.println("Uspeh ni naklučen!");
+        System.out.println("To je SERŠ!!");
     }
 }
