8697c8cf6bccf65b50bf842f50feedb0b05888a9
Tim <tim.prapotnik@sers.si>
2019-09-18 14:33:50 +0200

    Vaja d2

---
+++ ura3/src/main/java/adx/audioxd/ura3/Main.java
@@ -0,0 +1,21 @@
+package adx.audioxd.ura3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String... args) {
+        System.out.print("Vnesi 5 števil: ");
+
+        Scanner input = new Scanner(System.in);
+        int single = 0, multi = 0;
+        for (int i = 0; i < 5; i++) {
+            int n = input.nextInt();
+            if (n <= 9 && n >= -9) {
+                single++;
+            } else {
+                multi++;
+            }
+        }
+        System.out.println("Enomestnih je: " + single + ", Večmestnih pa: " + multi);
+    }
+}
