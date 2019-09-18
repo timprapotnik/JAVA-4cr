612701b95de62ecbf1f14499e43af8a8bd762e2b
Tim <tim.prapotnik@sers.si>
2019-09-18 15:05:10 +0200

    Vaja d2

--- ura3/src/main/java/adx/audioxd/ura3/Main.java
+++ ura3/src/main/java/adx/audioxd/ura3/Main.java
@@ -7,15 +7,18 @@ public class Main {
         System.out.println("Vnesi 5 števil: ");
 
         Scanner input = new Scanner(System.in);
-        int single = 0, multi = 0;
+        int single = 0, duble = 0, multi = 0;
         for (int i = 0; i < 5; i++) {
             int n = input.nextInt();
-            if (n <= 9 && n >= -9) {
+            if (n < 10 && n > -10) {
                 single++;
+            }
+            if (n < 100 && n > -100) {
+                duble++;
             } else {
                 multi++;
             }
         }
-        System.out.println("Enomestnih je: " + single + ", Večmestnih pa: " + multi);
+        System.out.println("Enomestnih je: " + single + ", Dvomestnih je: " + duble + ", Večmestnih pa: " + multi);
     }
 }
