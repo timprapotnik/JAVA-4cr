4ed228801d2a66b18b2670665b3de9084c963d19
Tim Prapotnik <tim.prapotnik@sers.si>
2019-10-18 09:45:28 +0200

    Vaja 5

--- ura5/src/main/java/adx/audioxd/ura5/Main.java
+++ ura5/src/main/java/adx/audioxd/ura5/Main.java
@@ -3,7 +3,51 @@ package adx.audioxd.ura5;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String... args) {
+    public static class Animal {
+        public Animal() {
+        }
 
+        public void talk() {
+            System.out.println("Grrr");
+        }
+
+        public void walk() {
+            System.out.println("I'm walking");
+        }
+    }
+
+    public static class Dog extends Animal {
+        @Override
+        public void talk() {
+            System.out.println("Woff Woff");
+        }
+    }
+
+    public static class Cat extends Animal {
+        @Override
+        public void talk() {
+            System.out.println("Meow Meow");
+        }
+    }
+
+    public static class Fox extends Animal {
+        @Override
+        public void talk() {
+            System.out.println("Ra-ta-ta-ta-ta-ta-ta");
+        }
+    }
+
+
+    public static void main(String... args) {
+        Animal[] animals = new Animal[]{
+                new Animal(),
+                new Dog(),
+                new Cat(),
+                new Fox()
+        };
+
+        for (Animal a : animals) {
+            a.talk();
+        }
     }
 }
